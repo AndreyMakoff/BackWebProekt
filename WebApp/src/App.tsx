e2e66a7051437e4d6d65ed5261901e6bc25b60c8
Andrey <makiavelli.1986@mail.ru>
2025-01-08 21:02:44 +0400

    Loadsh zod

--- WebApp/src/App.tsx
+++ WebApp/src/App.tsx
@@ -1,11 +1,29 @@
-import './App.css';
-import { TrpcProvider } from './lib/trcp.tsx';
-import { AllIdeasPage } from './pages/AllIdeasPage/index.tsx';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout/index.tsx";
+import {
+  getAllIdeasRoute,
+  getViewIdeaRoute,
+  viewIdeaRouteParams,
+} from "./lib/routes";
+import { TrpcProvider } from "./lib/trcp.tsx";
+import { AllIdeasPage } from "./pages/AllIdeasPage/index.tsx";
+import { ViewIdeaPage } from "./pages/ViewIdeaPage/index.tsx";
 
 export const App = () => {
-	return (
-		<TrpcProvider>
-			<AllIdeasPage />
-		</TrpcProvider>
-	);
+  return (
+    <TrpcProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Layout />}>
+            <Route path={getAllIdeasRoute()} element={<AllIdeasPage />} />
+            <Route
+              path={getViewIdeaRoute(viewIdeaRouteParams)}
+              element={<ViewIdeaPage />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </TrpcProvider>
+  );
 };
--- WebApp/src/pages/AllIdeasPage/index.tsx
+++ WebApp/src/pages/AllIdeasPage/index.tsx
@@ -1,26 +1,31 @@
-import { trpc } from '../../lib/trcp.tsx';
-
+import { Link } from "react-router-dom";
+import { getViewIdeaRoute } from "../../lib/routes";
+import { trpc } from "../../lib/trcp.tsx";
 export const AllIdeasPage = () => {
-	const { data, error, isLoading, isFetching, isError } =
-		trpc.getIdeas.useQuery();
+  const { data, error, isLoading, isFetching, isError } =
+    trpc.getIdeas.useQuery();
 
-	if (isLoading || isFetching) {
-		return <span>Loading...</span>;
-	}
+  if (isLoading || isFetching) {
+    return <span>Loading...</span>;
+  }
 
-	if (isError) {
-		return <span>Error: {error.message}</span>;
-	}
+  if (isError) {
+    return <span>Error: {error.message}</span>;
+  }
 
-	return (
-		<div>
-			<h1>All Ideas</h1>
-			{data.ideas.map((idea) => (
-				<div key={idea.nick}>
-					<h2>{idea.name}</h2>
-					<p>{idea.description}</p>
-				</div>
-			))}
-		</div>
-	);
+  return (
+    <div>
+      <h1>All Ideas</h1>
+      {data.ideas.map((idea) => (
+        <div key={idea.nick}>
+          <h2>
+            <Link to={getViewIdeaRoute({ ideaNick: idea.nick })}>
+              {idea.name}
+            </Link>
+          </h2>
+          <p>{idea.description}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
