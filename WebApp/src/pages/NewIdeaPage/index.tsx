b8b21dd23ec175d7fc73cbdf04607cc8e96c341c
Andrey <makiavelli.1986@mail.ru>
2025-02-05 21:13:39 +0400

    Changing Form

--- WebApp/src/pages/NewIdeaPage/index.tsx
+++ WebApp/src/pages/NewIdeaPage/index.tsx
@@ -1,11 +1,20 @@
+/* eslint-disable @typescript-eslint/no-restricted-imports */
+import { zCreateIdeaTrpcInput } from "@ideanick/backend/src/router/createIdea/input.ts";
 import { useFormik } from "formik";
 import { withZodSchema } from "formik-validator-zod";
-import { z } from "zod";
+import { useState } from "react";
+import { Alert } from "../../components/Alert";
+import { Button } from "../../components/Button";
+import { FormItems } from "../../components/FormItems";
 import { Input } from "../../components/Input";
 import { Segment } from "../../components/Segment";
 import { Textarea } from "../../components/Textarea";
+import { trpc } from "../../lib/trcp.tsx";
 
 export const NewIdeaPage = () => {
+  const [successMessageVisible, setSuccessMessageVisible] = useState(false);
+  const [submittingError, setSubmittingError] = useState<string | null>(null);
+  const createIdea = trpc.createIdea.useMutation();
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -13,24 +22,21 @@ export const NewIdeaPage = () => {
       description: "",
       text: "",
     },
-    validate: withZodSchema(
-      z.object({
-        name: z.string().min(1),
-        nick: z
-          .string()
-          .min(1)
-          .regex(
-            /^[a-z0-9-]+$/,
-            "Nick may contain only lowercase letters, numbers and dashes",
-          ),
-        description: z.string().min(1),
-        text: z
-          .string()
-          .min(100, "Text should be at least 100 characters long"),
-      }),
-    ),
-    onSubmit: (values) => {
-      console.info("Submitted", values);
+    validate: withZodSchema(zCreateIdeaTrpcInput),
+    onSubmit: async (values) => {
+      try {
+        await createIdea.mutateAsync(values);
+        formik.resetForm();
+        setSuccessMessageVisible(true);
+        setTimeout(() => {
+          setSuccessMessageVisible(false);
+        }, 3000);
+      } catch (error: any) {
+        setSubmittingError(error.message);
+        setTimeout(() => {
+          setSubmittingError(null);
+        }, 3000);
+      }
     },
   });
 
@@ -42,14 +48,23 @@ export const NewIdeaPage = () => {
           formik.handleSubmit();
         }}
       >
-        <Input name="name" label="Name" formik={formik} />
-        <Input name="nick" label="Nick" formik={formik} />
-        <Input name="description" label="Description" formik={formik} />
-        <Textarea name="text" label="Text" formik={formik} />
-        {!formik.isValid && !!formik.submitCount && (
-          <div style={{ color: "red" }}>Some fields are invalid</div>
-        )}
-        <button type="submit">Create Idea</button>
+        <FormItems>
+          <Input name="name" label="Name" formik={formik} />
+          <Input name="nick" label="Nick" formik={formik} />
+          <Input
+            name="description"
+            label="Description"
+            formik={formik}
+            maxWidth={500}
+          />
+          <Textarea name="text" label="Text" formik={formik} />
+          {!formik.isValid && !!formik.submitCount && (
+            <div style={{ color: "red" }}>Some fields are invalid</div>
+          )}
+          {!!submittingError && <Alert color="red">{submittingError}</Alert>}
+          {successMessageVisible && <Alert color="green">Idea created!</Alert>}
+          <Button loading={formik.isSubmitting}>Create Idea</Button>
+        </FormItems>
       </form>
     </Segment>
   );
